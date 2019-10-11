ee9875ce117a0adf968fd4b5fd876b688159afc8
imthaghost <imthaghost@protonmail.com>
2019-10-10 19:38:21 -0700

    arrrrggggg im a pirate

--- logger.py
+++ logger.py
@@ -5,61 +5,55 @@ from virus import Virus
 
 class Logger(object):
     ''' Utility class responsible for logging all interactions during the simulation. '''
-    # TODO: Write a test suite for this class to make sure each method is working
-    # as expected.
-
-    # PROTIP: Write your tests before you solve each function, that way you can
-    # test them one by one as you write your class.
 
     def __init__(self, file_name):
         self.file_name = file_name
 
-    def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate,
-                       repro_rate):
+    def write_metadata(self, *args):
         '''
         The simulation class should use this method immediately to log the specific
         parameters of the simulation as the first line of the file.
         '''
         with open(self.file_name, 'w') as f:
-            msg = f'Population Size: {pop_size}    Vaccination Percentage: {vacc_percentage}    Virus: {virus_name}    Mortality Rate: {mortality_rate}    Basic Reproduction Number: {repro_rate}\n'
+            msg = f'Population Size: {args[0]}    Vaccination Percentage: {args[1]}    Virus: {args[2]}    Mortality Rate: {args[3]}    Basic Reproduction Number: {args[4]}\n'
             f.write(msg)
 
-    def log_interaction(self, person, random_person, random_person_sick=None,
+    def log_interaction(self, *args, random_person_sick=None,
                         random_person_vacc=None, did_infect=None):
         '''
-        The Simulation object should use this method to log every interaction 
+        The Simulation object should use this method to log every interaction
         a sick person has during each time step.
         '''
         with open(self.file_name, 'a') as f:
             if did_infect and not random_person_vacc and not random_person_sick:
-                msg = f"{person._id} infects {random_person._id}.\n"
+                msg = f"{args[0]._id} infects {args[1]._id}.\n"
                 f.write(msg)
             elif not did_infect:
                 if random_person_vacc:
-                    msg = f"{person._id} didn't infect {random_person._id} because they are already vaccinated.\n"
+                    msg = f"{args[0]._id} didn't infect {args[1]._id} because they are already vaccinated.\n"
                     f.write(msg)
                 elif random_person_sick:
-                    msg = f"{person._id} didn't infect {random_person._id} because they are already sick.\n"
+                    msg = f"{args[0]._id} didn't infect {args[1]._id} because they are already sick.\n"
                     f.write(msg)
                 else:
-                    msg = f"{person._id} didn't infect {random_person._id} because they resisted the virus.\n"
+                    msg = f"{args[0]._id} didn't infect {args[1]._id} because they resisted the virus.\n"
                     f.write(msg)
 
-    def log_infection_survival(self, person, did_survive_infection):
+    def log_infection_survival(self, *args):
         ''' The Simulation object uses this method to log the results of every
         call of a Person object's .did_survive_infection() method.
         The format of the log should be:
             "{person.ID} died from infection\n" or "{person.ID} survived infection.\n"
         '''
         with open(self.file_name, 'a') as f:
-            if did_survive_infection:
-                msg = f"{person._id} survived infection.\n"
+            if args[1]:
+                msg = f"{args[0]._id} survived infection.\n"
                 f.write(msg)
-            elif not did_survive_infection:
-                msg = f"{person._id} died from infection.\n"
+            elif not args[1]:
+                msg = f"{args[0]._id} died from infection.\n"
                 f.write(msg)
 
-    def log_time_step(self, time_step_number):
+    def log_time_step(self, *args):
         ''' STRETCH CHALLENGE DETAILS:
         If you choose to extend this method, the format of the summary statistics logged
         are up to you.
@@ -72,5 +66,5 @@ class Logger(object):
             "Time step {time_step_number} ended, beginning {time_step_number + 1}\n"
         '''
         with open(self.file_name, 'a') as f:
-            msg = f"Time step {time_step_number} ended, beginning {time_step_number + 1}\n"
+            msg = f"Time step {args[0]} ended, beginning {args[0] + 1}\n"
             f.write(msg)
--- person.py
+++ person.py
@@ -6,9 +6,7 @@ class Person(object):
     ''' Person objects will populate the simulation. '''
 
     def __init__(self, _id, is_vaccinated, infection=None):
-        ''' We start out with is_alive = True, because we don't make vampires or zombies.
-        All other values will be set by the simulation when it makes each Person object.
-
+        ''' 
         If person is chosen to be infected when the population is created, the simulation
         should instantiate a Virus object and set it as the value
         self.infection. Otherwise, self.infection should be set to None.
@@ -24,20 +22,15 @@ class Person(object):
         If Person survives, they become vaccinated and they have no infection.
         Return a boolean value indicating whether they survived the infection.
         '''
-        # Only called if infection attribute is not None.
-        # TODO:  Finish this method. Should return a Boolean
         if self.infection:
-            rand_val = random()
-            if rand_val < self.infection.mortality_rate:
+            god = random()
+            if god < self.infection.mortality_rate:
                 self.is_alive = False
             else:
                 self.is_vaccinated = True
                 self.infection = None
 
         return self.is_alive
-
-
-''' These are simple tests to ensure that you are instantiating your Person class correctly. '''
 
 
 def test_vacc_person_instantiation():
@@ -64,7 +57,7 @@ def test_sick_person_instantiation():
 
 
 def test_did_survive_infection():
-    virus = Virus("Dysentery", 0.7, 0.2)
+    virus = Virus("HIV", 0.8, 0.2)
     person = Person(4, False, virus)
 
     # Resolve whether the Person survives the infection or not
@@ -83,3 +76,5 @@ def test_did_survive_infection():
 
 if __name__ == "__main__":
     test_vacc_person_instantiation()
+    test_not_vacc_person_instantiation()
+    test_sick_person_instantiation()
